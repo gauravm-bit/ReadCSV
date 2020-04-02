b676007db3434031de58d65098038eeba5cc407d
gauravm-bit <gauravwrk01@gmail.com>
2020-04-02 20:38:52 +0530

    ADDED: POJO class for reading csv directly.

---
+++ src/main/java/CSVUser.java
@@ -0,0 +1,24 @@
+import com.opencsv.bean.CsvBindByName;
+
+public class CSVUser {
+    @CsvBindByName
+    private String name;
+
+    @CsvBindByName(column = "email", required = true)
+    private String email;
+
+    @CsvBindByName(column = "phone")
+    private String phoneNo;
+
+    @CsvBindByName
+    private String country;
+
+    @Override
+    public String toString() {
+        return "CSVUser{" +
+                "name='" + name + '\''+
+                ", email='" + email + '\'' +
+                ", country='" + country + '\'' +
+                '}';
+    }
+}
--- src/main/java/OpenCSVReader.java
+++ src/main/java/OpenCSVReader.java
@@ -3,31 +3,28 @@ import java.io.IOException;
 import java.io.Reader;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.List;
+
 
 public class OpenCSVReader {
     private static final String SAMPLE_CSV_FILE_PATH = "src/main/resources/users.csv";
 
-    public static void main(String[] args) throws IOException {
-        readAllAtOnce();
-    }
-
-        private static void readAllAtOnce() throws IOException {
+    public static void main(String[] args) {
             try (
                     Reader reader = Files.newBufferedReader(Paths.get(SAMPLE_CSV_FILE_PATH));
                     CSVReader csvReader = new CSVReader(reader);
                     ) {
                     // Reading Records all at once in a String array
-                    List<String[]> records = csvReader.readAll();
-                    for (String[] record : records) {
-                        System.out.println("Name : " + record[0]);
-                        System.out.println("Email : " + record[1]);
-                        System.out.println("Phone : " + record[2]);
-                        System.out.println("Country : " + record[3]);
+                    String[] nextRecord;
+                    while ((nextRecord = csvReader.readNext()) != null) {
+                        System.out.println("Name : " + nextRecord[0]);
+                        System.out.println("Email : " + nextRecord[1]);
+                        System.out.println("Phone : " + nextRecord[2]);
+                        System.out.println("Country : " + nextRecord[3]);
                         System.out.println("==========================");
 
                     }
-
+            } catch (IOException e){
+                e.printStackTrace();
             }
         }
     }
