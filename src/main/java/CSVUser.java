f961153c8e0c4eb86a78ab010de701e1c740bb89
gauravm-bit <gauravwrk01@gmail.com>
2020-04-03 10:42:40 +0530

    ADDED: Parse PSV into POJO.

--- src/main/java/CSVUser.java
+++ src/main/java/CSVUser.java
@@ -13,7 +13,38 @@ public class CSVUser {
     @CsvBindByName
     private String country;
 
-    @Override
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhoneNo() {
+        return phoneNo;
+    }
+
+    public void setPhoneNo(String phoneNo) {
+        this.phoneNo = phoneNo;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
     public String toString() {
         return "CSVUser{" +
                 "name='" + name + '\''+
--- src/main/java/OpenCSVReader.java
+++ src/main/java/OpenCSVReader.java
@@ -1,30 +1,34 @@
-import com.opencsv.CSVReader;
+import com.opencsv.bean.CsvToBean;
+import com.opencsv.bean.CsvToBeanBuilder;
 import java.io.IOException;
 import java.io.Reader;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.Iterator;
 
 
 public class OpenCSVReader {
     private static final String SAMPLE_CSV_FILE_PATH = "src/main/resources/users.csv";
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
             try (
                     Reader reader = Files.newBufferedReader(Paths.get(SAMPLE_CSV_FILE_PATH));
-                    CSVReader csvReader = new CSVReader(reader);
                     ) {
-                    // Reading Records all at once in a String array
-                    String[] nextRecord;
-                    while ((nextRecord = csvReader.readNext()) != null) {
-                        System.out.println("Name : " + nextRecord[0]);
-                        System.out.println("Email : " + nextRecord[1]);
-                        System.out.println("Phone : " + nextRecord[2]);
-                        System.out.println("Country : " + nextRecord[3]);
+                     CsvToBean<CSVUser> csvToBean = new CsvToBeanBuilder(reader)
+                             .withType(CSVUser.class)
+                             .withIgnoreLeadingWhiteSpace(true)
+                             .build();
+                     Iterator csvUserIterator = csvToBean.iterator();
+
+                    while (csvUserIterator.hasNext()) {
+                        CSVUser csvUser = (CSVUser) csvUserIterator.next();
+                        System.out.println("Name : " + csvUser.getName());
+                        System.out.println("Email : " + csvUser.getEmail());
+                        System.out.println("Phone : " + csvUser.getPhoneNo());
+                        System.out.println("Country : " + csvUser.getCountry());
                         System.out.println("==========================");
 
                     }
-            } catch (IOException e){
-                e.printStackTrace();
             }
         }
     }
